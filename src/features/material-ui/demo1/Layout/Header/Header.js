ebf3f34b4120540fffb354c8ca1288cc5f095bd6
Jaswant Mandloi <jaswantmandloi@gmail.com>
2021-01-04 20:48:47 +0530

    Update header

--- src/features/material-ui/demo1/Layout/Header/Header.js
+++ src/features/material-ui/demo1/Layout/Header/Header.js
@@ -1,10 +1,15 @@
-import Container from "@material-ui/core/Container";
+//import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Input from "@material-ui/core/Input";
 import InputAdornment from "@material-ui/core/InputAdornment";
-import SearchIcon from "@material-ui/icons/Search";
 import Paper from "@material-ui/core/Paper";
 import Link from "@material-ui/core/Link";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import SearchIcon from "@material-ui/icons/Search";
+import HelpOutline from "@material-ui/icons/HelpOutline";
+import ShoppingCart from "@material-ui/icons/ShoppingCart";
+import ExpandMore from "@material-ui/icons/ExpandMore";
 
 import {
   useStylesContainer,
@@ -13,16 +18,7 @@ import {
   useStylesSearch,
   useStylesMenus,
 } from "./HeaderStyles";
-
-const getBreakPointsWidths = (maxWidth) => {
-  return {
-    xm: maxWidth,
-    sm: maxWidth,
-    md: maxWidth,
-    lg: maxWidth,
-    xl: maxWidth,
-  };
-};
+import { getBreakPointsWidths } from "features/material-ui/demo1/Utils";
 
 export default function Header() {
   const containerClasses = useStylesContainer();
@@ -31,57 +27,75 @@ export default function Header() {
   const searchClasses = useStylesSearch();
   const menusClasses = useStylesMenus();
   return (
-    <Container fixed className={containerClasses.root}>
-      <Paper className={containerClasses.paper}>
-        <Grid container alignItems="center" styles={{ flexGrow: 1 }}>
-          <Grid item {...getBreakPointsWidths(2)} className={logoClasses.logo}>
-            {/* <img src="/demo1/images/logo.svg" className={logoClasses.logoImage} /> */}
-            <img
-              src="/demo1/images/white_logo.svg"
-              className={logoClasses.logoImage}
-            />
-          </Grid>
-          <Grid item {...getBreakPointsWidths(3)}>
-            <Grid container className={megaMenuClasses.container}>
-              <Grid item>Products</Grid>
-              <Grid item>Deals</Grid>
-              <Grid item>Services</Grid>
+    // <Container className={containerClasses.root}>
+    <Paper className={containerClasses.paper}>
+      <Grid container alignItems="center">
+        <Grid item {...getBreakPointsWidths(2)} className={logoClasses.logo}>
+          {/* <img src="/demo1/images/logo.svg" /> */}
+          <img src="/demo1/images/white_logo.svg" />
+        </Grid>
+        <Grid item {...getBreakPointsWidths(4)}>
+          <Grid container className={megaMenuClasses.container}>
+            <Grid item>
+              <Link href="#">
+                Products
+                <ExpandMore />
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#">
+                Deals <ExpandMore />
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#">
+                Services <ExpandMore />
+              </Link>
             </Grid>
           </Grid>
-
-          <Grid
-            item
-            {...getBreakPointsWidths(3)}
-            className={searchClasses.container}
-          >
-            <Paper className={searchClasses.paper}>
-              <Input
-                fullWidth
-                disableUnderline={true}
-                type="search"
-                placeholder="Search"
-                inputProps={{ "aria-label": "description" }}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <SearchIcon className={searchClasses.searchIcon} />
-                  </InputAdornment>
-                }
-              />
-            </Paper>
-          </Grid>
-
-          <Grid
-            item
-            {...getBreakPointsWidths(3)}
-            className={menusClasses.container}
-            justify="flex-end"
-          >
-            <Link href="#">Link</Link>
-            <Link href="#">Link</Link>
-            <Link href="#">Link</Link>
-          </Grid>
         </Grid>
-      </Paper>
-    </Container>
+
+        <Grid
+          item
+          {...getBreakPointsWidths(4)}
+          className={searchClasses.container}
+        >
+          <Paper className={searchClasses.paper}>
+            <Input
+              fullWidth
+              disableUnderline={true}
+              type="search"
+              placeholder="Search"
+              inputProps={{ "aria-label": "description" }}
+              endAdornment={
+                <InputAdornment position="end">
+                  <SearchIcon className={searchClasses.searchIcon} />
+                </InputAdornment>
+              }
+            />
+          </Paper>
+        </Grid>
+
+        <Grid
+          item
+          {...getBreakPointsWidths(2)}
+          className={menusClasses.container}
+          justify="flex-end"
+        >
+          <Box display="flex" justifyContent="space-around">
+            <Link href="#">
+              <Typography variant="subtitle2">Sign In</Typography>
+            </Link>
+            <Link href="#">
+              <HelpOutline />
+            </Link>
+            <Link href="#">
+              <ShoppingCart />
+            </Link>
+          </Box>
+        </Grid>
+      </Grid>
+    </Paper>
+    // </Container>
   );
 }
--- src/features/material-ui/demo1/Layout/Header/HeaderStyles.js
+++ src/features/material-ui/demo1/Layout/Header/HeaderStyles.js
@@ -13,14 +13,14 @@ export const useStylesContainer = makeStyles((theme) => ({
 
 export const useStylesLogo = makeStyles((theme) => ({
   logo: {
-    marginLeft: theme.spacing(2),
     padding: `${theme.spacing(2)}px 0`,
-  },
-  logoImage: {
-    height: "24px",
-    width: "134px",
-    minWidth: "134px",
-    backgroundRepeat: "no-repeat",
+    "& img": {
+      marginLeft: theme.spacing(2),
+      height: "24px",
+      width: "134px",
+      minWidth: "134px",
+      backgroundRepeat: "no-repeat",
+    },
   },
 }));
 
@@ -30,18 +30,25 @@ export const useStylesMegaMenus = makeStyles((theme) => ({
     "& div": {
       padding: `0 ${theme.spacing(2)}px `,
     },
+    "& a": {
+      color: theme.palette.text.light,
+      display: "flex",
+    },
   },
 }));
 
 export const useStylesSearch = makeStyles((theme) => ({
   container: {
-    paddingLeft: theme.spacing(5),
+    //paddingLeft: theme.spacing(5),
   },
 
   paper: {
     borderRadius: `${theme.shape.searchBoxBorderRadius}px`,
     "& input": {
-      paddingLeft: theme.spacing(1.25),
+      //paddingLeft: theme.spacing(1.25),
+      padding: `${theme.spacing(1.25)}px ${theme.spacing(
+        1.25
+      )}px ${theme.spacing(1.25)}px `,
     },
     "& div": {
       marginRight: theme.spacing(1.25),
@@ -57,6 +64,7 @@ export const useStylesMenus = makeStyles((theme) => ({
     display: "flex",
     "& a": {
       color: theme.palette.text.light,
+      margin: `0 ${theme.spacing(2)}px`,
     },
   },
 }));
