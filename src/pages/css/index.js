a1f331c4501712a7c98bead336f6b238d45c588e
Jaswant Mandloi <jaswantmandloi@gmail.com>
2021-05-11 20:01:09 +0530

    Create Homepage

--- src/pages/css/index.js
+++ src/pages/css/index.js
@@ -1,36 +1,26 @@
 import Head from "next/head";
-import Link from "next/link";
 import styles from "styles/css/index.module.css";
 
 export default function Index() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>CSS Demos</title>
+        <title>Home Page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.listContainer}>
-        {listItems.map((item) => {
-          return (
-            <div key={item.id} className={styles.listItem}>
-              <Link href={item.href}>
-                <a>{item.name}</a>
-              </Link>
-            </div>
-          );
-        })}
+      <div className={styles.container}>
+        <div className={styles.topHeader}>
+          <div className={styles.languageSelector}>
+            <select>
+              <option>En</option>
+              <option>Fr</option>
+            </select>
+          </div>
+          <div className={styles.liveChat}>
+            <button>Live Chat</button>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
-
-const listItems = [
-  { href: "#", id: 1, name: "First" },
-  { href: "#", id: 2, name: "second" },
-  { href: "#", id: 3, name: "Third" },
-  { href: "#", id: 4, name: "Forth" },
-  { href: "#", id: 5, name: "Fifth" },
-  { href: "#", id: 6, name: "Sixth" },
-  { href: "#", id: 7, name: "Seventh" },
-  { href: "#", id: 8, name: "Eighth" },
-];
