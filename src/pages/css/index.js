312b210038381d73c4186fd63d0819c9dbc87827
Jaswant Mandloi <jaswantmandloi@gmail.com>
2021-05-12 17:05:57 +0530

    Add header parts

--- src/pages/css/index.js
+++ src/pages/css/index.js
@@ -20,6 +20,29 @@ export default function Index() {
             <button>Live Chat</button>
           </div>
         </div>
+
+        <div className={styles.header}>
+          <img
+            src="/css/index/business advantage.svg"
+            className={styles.logo}
+          />
+          <div className={styles.headerMiddle}>
+            <div className={styles.searchBar}>
+              <input
+                type="search"
+                placeholder="Search for all business needs"
+              />
+              <button className={styles.search}>
+                <img src="/css/index/one_search.png" className={styles.logo} />
+              </button>
+            </div>
+            <div className={styles.inkToner}>
+              <button>
+                <img src="/css/index/drop@1x.png" /> &nbsp; Ink & Toner
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
