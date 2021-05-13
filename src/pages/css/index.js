fbb140d57675a577293ef2909efd7bac9deb2688
Jaswant Mandloi <jaswantmandloi@gmail.com>
2021-05-13 18:36:07 +0530

    Add mega menu items

--- src/pages/css/index.js
+++ src/pages/css/index.js
@@ -57,6 +57,13 @@ export default function Index() {
             </button>
           </div>
         </div>
+
+        <div className={styles.megaMenu}>
+          <button>PRODUCTS &darr;</button>
+          <button>Services & Solutions &darr;</button>
+          <button>Flyers & Publications </button>
+          <button>Become a customer </button>
+        </div>
       </div>
     </>
   );
