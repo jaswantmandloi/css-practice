3e5974539c42557959952d41454e3e5eca7a87e8
Jaswant Mandloi <jaswantmandloi@gmail.com>
2021-05-14 18:21:26 +0530

    Add responsiveness in the header

--- src/pages/css/index.js
+++ src/pages/css/index.js
@@ -10,14 +10,35 @@ export default function Index() {
       </Head>
       <div className={styles.container}>
         <div className={styles.topHeader}>
-          <div className={styles.languageSelector}>
-            <select>
-              <option>En</option>
-              <option>Fr</option>
-            </select>
+          <div className={styles.topHeaderFirstPart}>
+            <div className={styles.languageSelector}>
+              <select>
+                <option>En</option>
+                <option>Fr</option>
+              </select>
+            </div>
+            <div className={styles.logout}>
+              <button>Logout</button>
+            </div>
           </div>
-          <div className={styles.liveChat}>
-            <button>Live Chat</button>
+          <div className={styles.topHeaderSecondPart}>
+            <div className={styles.messages}>
+              <button>Messages</button>
+            </div>
+            <div className={styles.liveChat}>
+              <button>Live Chat</button>
+            </div>
+          </div>
+
+          <div className={styles.topHeaderLastPart}>
+            <div className={styles.shipToLocation}>
+              <span>Ship to location</span>
+              <span>*</span>
+              <span>Hamilton Employee (178318) &darr;</span>
+            </div>
+            <div className={styles.findAccount}>
+              <button>Find Account</button>
+            </div>
           </div>
         </div>
 
