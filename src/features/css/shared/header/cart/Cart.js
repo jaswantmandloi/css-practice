36b8e4df9491bbcc83f55cc9b241e060757361d7
Jaswant Mandloi <jaswantmandloi@gmail.com>
2021-06-20 18:21:28 +0530

    Add cart popup

--- src/features/css/shared/header/cart/Cart.js
+++ src/features/css/shared/header/cart/Cart.js
@@ -16,8 +16,45 @@ export default function Index() {
 
         <div className={`${styles.cart} ${isOpen ? styles.active : ""}`}>
           <div className={styles.header}>8 Items</div>
+          <div className={styles.body}>
+            {cartProducts.map((product) => {
+              const { productCode, quantity } = product;
+              return (
+                <div key={productCode} className={styles.productItem}>
+                  <div>{productCode}</div>
+                  <div>{quantity}</div>
+                </div>
+              );
+            })}
+            <div className={styles.total}>
+              <div className={styles.totalItems}>+5 more items</div>
+              <div className={styles.orderTotal}>
+                <div>Order total :</div>
+                <div>$92.46</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.footer}>
+            <div className={styles.actions}>
+              <button>Checkout</button>
+              <button>View cart</button>
+            </div>
+            <div className={styles.linkDescription}>
+              <a href="#">Click here</a> to add a catalogue
+            </div>
+            <div className={styles.linkDescription}>
+              <a href="#">Click here</a> for printable version
+            </div>
+          </div>
         </div>
       </button>
     </>
   );
 }
+
+const cartProducts = [
+  { productCode: "DMNC181731", quantity: 1 },
+  { productCode: "DMNC181732", quantity: 100 },
+  { productCode: "DMNC181733", quantity: 2 },
+  { productCode: "DMNC181734", quantity: 5 },
+];
